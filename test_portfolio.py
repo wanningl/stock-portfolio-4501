8be4a02116a7c5809f7926a7a00c05e18a8dc23a
wanningl <wl2826@columbia.edu>
2021-11-18 17:39:18 -0500

    add some tests

--- test_portfolio.py
+++ test_portfolio.py
@@ -1,6 +1,25 @@
-from portfolio import Portfolio
+import pytest
 
+from portfolio import Portfolio
+def test_empty_portfolio():
+    p = Portfolio()
+    assert p.cost()==0.0
 def test_buy_one_stock():
     p = Portfolio()
     p.buy("IBM", 100, 176.48)
     assert p.cost() == 17648.0
+def test_buy_two_stocks():
+    p = Portfolio()
+    p.buy("IBM", 100, 176.48)
+    p.buy("HPQ", 100, 36.15)
+    assert p.cost()==21263
+
+def test_not_enough_arguments_to_buy():
+    p = Portfolio()
+    with pytest.raises(TypeError):
+        p.buy("IBM", price=100)
+        
+    # except TypeError:
+    #     pass
+    # else:
+    #     assert False, "We didn't get an exception"
