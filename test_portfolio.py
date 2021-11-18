7c23c1f135f10f83f7089dbe5da7e1b8d99b4c70
wanningl <wl2826@columbia.edu>
2021-11-18 16:47:52 -0500

    Update test_portfolio.py

--- test_portfolio.py
+++ test_portfolio.py
@@ -6,6 +6,3 @@ assert p.cost() == 0.0
 p.buy("IBM", 100, 176.48)
 print(f"With 100 IBM @ 176.48: {p.cost()}, should be 17648.0")
 assert p.cost() == 17648.0
-p.buy("HPQ", 100, 36.15)
-print(f"With 100 HPQ @ 36.15: {p.cost()}, should be 21263.0")
-assert p.cost() == 21263.0
